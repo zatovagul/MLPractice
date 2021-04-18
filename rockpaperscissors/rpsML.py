57691fb18e4ffcfdfb5c1a436aaaa1685e9dc176
temirlan <zatovagul@gmail.com>
2021-04-18 21:11:36 +0300

    New m

--- rockpaperscissors/rpsML.py
+++ rockpaperscissors/rpsML.py
@@ -1,18 +1,36 @@
 import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 rock_dir = os.path.join('/tmp/rps/rock')
 paper_dir = os.path.join('/tmp/rps/paper')
 scissors_dir = os.path.join('/tmp/rps/scissors')
 
-print('total training rock images:', len(os.listdir(rock_dir)))
-print('total training paper images:', len(os.listdir(paper_dir)))
-print('total training scissors images:', len(os.listdir(scissors_dir)))
+# print('total training rock images:', len(os.listdir(rock_dir)))
+# print('total training paper images:', len(os.listdir(paper_dir)))
+# print('total training scissors images:', len(os.listdir(scissors_dir)))
 
 rock_files = os.listdir(rock_dir)
-print(rock_files[:10])
+# print(rock_files[:10])
 
 paper_files = os.listdir(paper_dir)
-print(paper_files[:10])
+# print(paper_files[:10])
 
 scissors_files = os.listdir(scissors_dir)
-print(scissors_files[:10])
+# print(scissors_files[:10])
+
+pic_index = 2
+
+next_rock = [os.path.join(rock_dir, fname)
+             for fname in rock_files[pic_index - 2:pic_index]]
+next_paper = [os.path.join(paper_dir, fname)
+              for fname in paper_files[pic_index - 2:pic_index]]
+next_scissors = [os.path.join(scissors_dir, fname)
+                 for fname in scissors_files[pic_index - 2:pic_index]]
+
+for i, img_path in enumerate(next_rock + next_paper + next_scissors):
+    print(img_path)
+    img = mpimg.imread(img_path)
+    plt.imshow(img)
+    plt.axis('Off')
+    plt.show()
